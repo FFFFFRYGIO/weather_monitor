febb3a3331c52b0057b2a7142322f693bf636b75
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-06-16 01:27:09 +0200

    update listener to use setWeatherPromptData method

--- src/main/java/com/example/weather_monitor/listener/WeatherUpdatesListener.java
+++ src/main/java/com/example/weather_monitor/listener/WeatherUpdatesListener.java
@@ -1,16 +1,21 @@
 package com.example.weather_monitor.listener;
 
+import com.example.weather_monitor.WeatherMonitorController;
 import com.example.weather_monitor.db.DBManager;
 import com.example.weather_monitor.event.WeatherRecordEvent;
 import com.google.common.eventbus.Subscribe;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 /* Listener for weather registers parsed to save them in database and update it on the map */
 public class WeatherUpdatesListener {
     private static int eventsHandled = 0;
+    private static WeatherMonitorController weatherMonitorController;
+
+    public WeatherUpdatesListener(WeatherMonitorController weatherMonitorController) {
+        this.weatherMonitorController = weatherMonitorController;
+    }
 
     @Subscribe
     public void handleEvent(WeatherRecordEvent event) {
@@ -20,6 +25,6 @@ public class WeatherUpdatesListener {
         } catch (IOException | SQLException e) {
             throw new RuntimeException(e);
         }
-
+        weatherMonitorController.setWeatherPromptData(event.weatherRecord());
     }
 }
